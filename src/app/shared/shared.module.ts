3211ffa3682fbd5ca36571dceee148e3d2dcd3a0
Hulford <moraleshulford@gmail.com>
2023-11-21 00:21:00 -0500

    Resolucion de la tarea

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -12,7 +13,8 @@ import { RouterModule } from '@angular/router';
 
     HomePageComponent,
      AboutPageComponent,
-     SidebarComponent
+     SidebarComponent,
+     ContactPageComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +23,8 @@ import { RouterModule } from '@angular/router';
   exports:[
     HomePageComponent,
     AboutPageComponent,
-    SidebarComponent
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
