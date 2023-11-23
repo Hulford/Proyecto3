785ed7b14a6b1e42d1541982625112c7a1bb2c17
Hulford <moraleshulford@gmail.com>
2023-11-23 02:19:22 -0500

    Buscar por region - Optimizacion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+
+type Region = 'Africa'|'America'|'Asia'|'Europa'|'Oceania';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,11 +12,14 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[]=[];
+  public regions: Region[]= ['Africa', 'America', 'Asia', 'Europa', 'Oceania'];
+  public selectedRegion?: Region;
   constructor(private countriesService: CountriesService){
 
   }
-  searchByRegion(term: string):void{
-    this.countriesService.searchRegion(term)
+  searchByRegion(region: Region):void{
+    this.selectedRegion= region;
+        this.countriesService.searchRegion(region)
     .subscribe(countries =>{
       this.countries=countries;
     });
